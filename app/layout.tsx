2313b390ea330ca83b92250f7a05d8c6a343e034
Michael P.F.C. <maiconz3686@gmail.com>
2026-02-14 20:39:23 -0300

    feat: initialize Next.js project with TypeScript and Tailwind CSS

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-sans'});
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "ExplainMyBigO",
+  description: "ExplainMyBigO is a simple AI-powered tool that estimates the time and space complexity (Big-O) of your code, highlights key assumptions, and gives a short, clear explanation.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={cn(jetbrainsMono.variable, "dark")}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
